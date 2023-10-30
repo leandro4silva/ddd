fdd5f58c326502d3df7b68be0e1c9c4bf364a5f3
leandro4silva <leandro94.a.s@gmail.com>
2023-10-30 20:49:37 -0300

    feat: add methods to answer repository

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,12 @@
+import { PaginatinParams } from "@/core/repositories/pagination-params";
 import { Answer } from "../../enterprise/entities/answer";
 
 export interface AnswersRespository {
   findById(id: string): Promise<Answer | null>;
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginatinParams,
+  ): Promise<Answer[]>;
   create(answer: Answer): Promise<void>;
   delete(answer: Answer): Promise<void>;
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,8 +1,20 @@
+import { PaginatinParams } from "@/core/repositories/pagination-params";
 import { AnswersRespository } from "@/domain/forum/application/repositories/answers-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 export class InMemoryAnswersRepository implements AnswersRespository {
   public items: Answer[] = [];
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginatinParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answers;
+  }
 
   async findById(id: string): Promise<Answer | null> {
     const answer = this.items.find((item) => item.id.toString() === id);
